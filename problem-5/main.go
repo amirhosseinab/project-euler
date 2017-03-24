124f2c3f2c8b001930c4783bedb3bdd1430d22a2
amirhosseinab <ah.abdolkhalegh@gmail.com>
2017-03-24 17:18:49 +0430

    Implement solution

--- problem-5/main.go
+++ problem-5/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const md int = 20
+	fmt.Println(findSmallestEvenlyDivisible(md))
+}
+
+func findSmallestEvenlyDivisible(md int) int {
+	var sd int = md
+	var succeeded bool
+
+	for {
+		sd++
+		succeeded = true
+		for m := 1; m <= md; m++ {
+			if sd%m != 0 {
+				succeeded = false
+				break
+			}
+		}
+		if succeeded {
+			break
+		}
+	}
+	return sd
 }
